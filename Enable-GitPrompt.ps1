97d5739bf508e38dc210a44c905402645a7170a5
James Kovacs <jkovacs@post.harvard.edu>
2010-03-20 15:05:08 -0600

    Changed names to better match PS naming conventions.

--- Enable-GitPrompt.ps1
+++ Enable-GitPrompt.ps1
@@ -2,8 +2,6 @@
 # http://www.markembling.info/view/my-ideal-powershell-prompt-with-git-integration
 # Displays git branch and stats when inside a git repository.
 
-. (Resolve-Path ~/Documents/WindowsPowershell/gitutils.ps1)
-
 function prompt {
 	$path = ""
 	$pathbits = ([string]$pwd).split("\", [System.StringSplitOptions]::RemoveEmptyEntries)
@@ -41,4 +39,81 @@ function prompt {
     
 	Write-Host('>') -nonewline -foregroundcolor Green    
 	return " "
+}
+
+# Git functions
+# Mark Embling (http://www.markembling.info/)
+# http://www.markembling.info/view/my-ideal-powershell-prompt-with-git-integration
+
+# Is the current directory a git repository/working copy?
+function isCurrentDirectoryGitRepository {
+    if ((Test-Path ".git") -eq $TRUE) {
+        return $TRUE
+    }
+    
+    # Test within parent dirs
+    $checkIn = (Get-Item .).parent
+    while ($checkIn -ne $NULL) {
+        $pathToTest = $checkIn.fullname + '/.git'
+        if ((Test-Path $pathToTest) -eq $TRUE) {
+            return $TRUE
+        } else {
+            $checkIn = $checkIn.parent
+        }
+    }
+    
+    return $FALSE
+}
+
+# Get the current branch
+function gitBranchName {
+    $currentBranch = ''
+    git branch | foreach {
+        if ($_ -match "^\* (.*)") {
+            $currentBranch += $matches[1]
+        }
+    }
+    return $currentBranch
+}
+
+# Extracts status details about the repo
+function gitStatus {
+    $untracked = $FALSE
+    $added = 0
+    $modified = 0
+    $deleted = 0
+    $ahead = $FALSE
+    $aheadCount = 0
+    
+    $output = git status
+    
+    $branchbits = $output[0].Split(' ')
+    $branch = $branchbits[$branchbits.length - 1]
+    
+    $output | foreach {
+        if ($_ -match "^\#.*origin/.*' by (\d+) commit.*") {
+            $aheadCount = $matches[1]
+            $ahead = $TRUE
+        }
+        elseif ($_ -match "deleted:") {
+            $deleted += 1
+        }
+        elseif (($_ -match "modified:") -or ($_ -match "renamed:")) {
+            $modified += 1
+        }
+        elseif ($_ -match "new file:") {
+            $added += 1
+        }
+        elseif ($_ -match "Untracked files:") {
+            $untracked = $TRUE
+        }
+    }
+    
+    return @{"untracked" = $untracked;
+             "added" = $added;
+             "modified" = $modified;
+             "deleted" = $deleted;
+             "ahead" = $ahead;
+             "aheadCount" = $aheadCount;
+             "branch" = $branch}
 }
